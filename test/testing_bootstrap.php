bbb72b71b2ba90f579a3ae9e31bba4fd639a30fd
Lukas Bestle <lukas@lu-x.me>
2013-02-23 09:25:16 +0100

    Fix __DIR__
    PHP 5.2 does not know __DIR__, so it's better to use dirname(__FILE__) instead.

--- test/testing_bootstrap.php
+++ test/testing_bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-if(!defined('TEST_CONTENT')) define('TEST_CONTENT', __DIR__ . '/testContent');
-if(!defined('TEST_SITE')) define('TEST_SITE', __DIR__ . '/testSite');
-if(!defined('TEST_KIRBY_CORE')) define('TEST_KIRBY_CORE', dirname(__DIR__));
+if(!defined('TEST_CONTENT')) define('TEST_CONTENT', dirname(__FILE__) . '/testContent');
+if(!defined('TEST_SITE')) define('TEST_SITE', dirname(__FILE__) . '/testSite');
+if(!defined('TEST_KIRBY_CORE')) define('TEST_KIRBY_CORE', dirname(dirname(__FILE__)));
 if(!defined('TEST_KIRBY_LIB')) define('TEST_KIRBY_LIB', TEST_KIRBY_CORE . '/lib');
 
 $roots = array(
-  'root'         => dirname(dirname(__DIR__)),
-  'root.kirby'   => dirname(__DIR__),
+  'root'         => dirname(dirname(dirname(__FILE__))),
+  'root.kirby'   => dirname(dirname(__FILE__)),
   'root.site'    => TEST_SITE,
   'root.content' => TEST_CONTENT
 );
