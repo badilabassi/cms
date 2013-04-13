2320c1fb3c9799d6807535dbf625dcf6f69d9c64
Bastian Allgeier <bastian.allgeier@gmail.com>
2013-04-13 22:06:46 +0200

    fixed the test setup

--- test/lib/testing_bootstrap.php
+++ test/lib/testing_bootstrap.php
@@ -5,12 +5,10 @@ if(!defined('TEST_SITE')) define('TEST_SITE', realpath(dirname(__FILE__) . '/../
 if(!defined('TEST_KIRBY_CORE')) define('TEST_KIRBY_CORE', realpath(dirname(__FILE__) . '/../..'));
 if(!defined('TEST_KIRBY_LIB')) define('TEST_KIRBY_LIB', TEST_KIRBY_CORE . '/lib');
 
-$roots = array(
-  'root'         => realpath(dirname(__FILE__) . '/../../..'),
-  'root.kirby'   => realpath(dirname(__FILE__) . '/../..'),
-  'root.site'    => TEST_SITE,
-  'root.content' => TEST_CONTENT
-);
+define('ROOT',         realpath(dirname(__FILE__) . '/../../..'));
+define('ROOT_KIRBY',   realpath(dirname(__FILE__) . '/../..')); 
+define('ROOT_SITE',    TEST_SITE);
+define('ROOT_CONTENT', TEST_CONTENT);  
 
 // include the kirby bootstrapper file
 require_once(TEST_KIRBY_CORE . '/bootstrap.php');
